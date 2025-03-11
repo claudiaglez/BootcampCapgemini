9b3e480ac4f9d4f4972a3a6809379dace765b083
claudiaglez <claudia.glezg@gmail.com>
2025-03-11 08:11:33 +0100

    Add CalculadoraTest to the project to learn JUnit

--- demo/src/test/java/com/example/util/CalculadoraTest.java
+++ demo/src/test/java/com/example/util/CalculadoraTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
@@ -27,12 +28,56 @@ class CalculadoraTest {
 	}
 
 	@Test
+	@DisplayName("Suma dos enteros")
 	void testSuma() {
 		var calc = new Calculadora();
 		
 		var actual = calc.suma(2, 3);
 		
 		assertEquals(5, actual);
+	}
+	@Test
+	@DisplayName("Suma dos enteros grandes")
+	void testSumaInt() {
+		var calc = new Calculadora();
+		
+		var actual = calc.suma(Integer.MAX_VALUE, 1);
+		assertEquals(5, actual);
+		
+		assertTrue(1.0 / 0 > 0);
+		assertEquals(5, 1.0 / 0);
+//		assertTrue(actual > 0);
+	}
+
+	@Test
+	@DisplayName("Suma dos reales")
+	void testSuma2() {
+		var calc = new Calculadora();
+		
+		var actual = calc.suma(0.1, 0.2);
+
+		assertEquals(0.3, actual);
+	}
+	@Test
+	@DisplayName("Suma dos reales: resta")
+	void testSuma3() {
+		var calc = new Calculadora();
+		
+		assertEquals(0.1, calc.suma(1, -0.9));
+	}
+	@Test
+	@DisplayName("Divide dos enteros")
+	void testDivide() {
+		var calc = new Calculadora();
+		
+		assertEquals(0.5, calc.divide(1.0, 2));
+	}
+	@Test
+	@DisplayName("Divide por cero")
+	void testDivide2() {
+		var calc = new Calculadora();
+		var ex = assertThrows(ArithmeticException.class, () -> calc.divide(1, 0));
+		assertEquals("/ by zero", ex.getMessage());
 	}
 
 }
