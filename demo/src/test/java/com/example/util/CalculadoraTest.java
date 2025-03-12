a8517ecbd88729f52afd22a7bde8633ee0d36a29
claudiaglez <claudia.glezg@gmail.com>
2025-03-12 08:57:35 +0100

    Modify Mockito imports to add the methods, change to static import

--- demo/src/test/java/com/example/util/CalculadoraTest.java
+++ demo/src/test/java/com/example/util/CalculadoraTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 import com.example.test.util.Smoke;
 
