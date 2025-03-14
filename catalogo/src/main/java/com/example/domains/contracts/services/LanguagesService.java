b5b2f89618efc45afd1bccd55cced43fc8b4b33e
claudiaglez <claudia.glezg@gmail.com>
2025-03-14 09:08:21 +0100

    Create Languages service and repository and the implementation

---
+++ catalogo/src/main/java/com/example/domains/contracts/services/LanguagesService.java
@@ -0,0 +1,10 @@
+package com.example.domains.contracts.services;
+
+import java.util.List;
+
+import com.example.domains.core.contracts.services.DomainService;
+import com.example.domains.entities.Language;
+
+public interface LanguagesService extends DomainService<Language, Integer> {
+	List<Language> obtenerIdiomas();
+}
---
+++ catalogo/src/main/java/com/example/domains/services/LanguagesServiceImpl.java
@@ -0,0 +1,60 @@
+package com.example.domains.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.domains.contracts.services.LanguagesService;
+import com.example.domains.entities.Language;
+import com.example.exceptions.DuplicateKeyException;
+import com.example.exceptions.InvalidDataException;
+import com.example.exceptions.NotFoundException;
+
+public class LanguagesServiceImpl implements LanguagesService {
+
+	public LanguagesServiceImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Language> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Language> getOne(Integer id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Language add(Language item) throws DuplicateKeyException, InvalidDataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Language modify(Language item) throws NotFoundException, InvalidDataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Language item) throws InvalidDataException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Language> obtenerIdiomas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
