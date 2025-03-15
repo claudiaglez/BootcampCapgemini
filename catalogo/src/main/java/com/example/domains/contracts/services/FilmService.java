835c88862eb388c7c34db2598b99b845d57c1fb9
claudiaglez <claudia.glezg@gmail.com>
2025-03-15 16:19:23 +0100

    Create method obtenerPeliculasPorFechaLanzamiento in FilmService

--- catalogo/src/main/java/com/example/domains/contracts/services/FilmService.java
+++ catalogo/src/main/java/com/example/domains/contracts/services/FilmService.java
@@ -1,5 +1,12 @@
 package com.example.domains.contracts.services;
 
-public interface FilmService {
+import java.util.List;
+
+import com.example.domains.core.contracts.services.DomainService;
+import com.example.domains.entities.Film;
+
+public interface FilmService extends DomainService<Film, Integer> {
+	
+	List<Film> obtenerPeliculasPorFechaLanzamiento(Short fecha);
 
 }
