a71e39d0b092243861ce132976ee8dcc674ba493
claudiaglez <claudia.glezg@gmail.com>
2025-03-15 17:18:51 +0100

    Add validations to the Category entity

--- catalogo/src/main/java/com/example/domains/entities/Category.java
+++ catalogo/src/main/java/com/example/domains/entities/Category.java
@@ -2,6 +2,10 @@ package com.example.domains.entities;
 
 import java.io.Serializable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -24,6 +28,9 @@ public class Category implements Serializable {
 	@Column(name="last_update", insertable=false, updatable=false, nullable=false)
 	private Timestamp lastUpdate;
 
+	@NotEmpty(message = "El nombre de la categoría no puede estar vacío")
+	@NotNull(message = "El nombre de la categoría no puede ser nulo")
+	@Size(min = 3, max = 25, message = "El nombre de la categoría debe tener entre 3 y 25 caracteres")
 	@Column(nullable=false, length=25)
 	private String name;
 
