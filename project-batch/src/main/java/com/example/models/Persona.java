a196c178d6b7634604da4c07e85dff12978b5cfb
claudiaglez <claudia.glezg@gmail.com>
2025-03-18 13:46:16 +0100

    Pass CSV to DB in the PersonasJobConfiguration

--- project-batch/src/main/java/com/example/models/Persona.java
+++ project-batch/src/main/java/com/example/models/Persona.java
@@ -1,11 +1,50 @@
 package com.example.models;
 
 public class Persona {
-public Persona(long id2, String string, String correo2, String ip2) {
-		// TODO Auto-generated constructor stub
-	}
-private long id;
-private String nombre, correo, ip;
-// Ctor(), Get/Set, toString(), …
+
+    public Persona(long id, String nombre, String correo, String ip) {
+        this.id = id;          
+        this.nombre = nombre;  
+        this.correo = correo;   
+        this.ip = ip;          
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    private long id;
+    private String nombre, correo, ip;
+
+
 }
+
 
--- project-batch/src/main/java/com/example/models/PersonaDTO.java
+++ project-batch/src/main/java/com/example/models/PersonaDTO.java
@@ -4,10 +4,16 @@ import lombok.Data;
 
 @Data
 public class PersonaDTO {
+	
 		private long id;
 		private String nombre, apellidos, correo, sexo, ip;
 		// Ctor(), Get/Set, toString()
+		
+
+public PersonaDTO() {
+}
 		}
+
 
 
 
