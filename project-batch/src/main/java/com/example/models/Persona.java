5c42bb0d558aafd32811b740ae6ffa862299e281
claudiaglez <claudia.glezg@gmail.com>
2025-03-18 12:58:33 +0100

    Add PersonaItemProcessor, and PersonasJobListener to the project

--- project-batch/src/main/java/com/example/models/Persona.java
+++ project-batch/src/main/java/com/example/models/Persona.java
@@ -1,6 +1,9 @@
 package com.example.models;
 
 public class Persona {
+public Persona(long id2, String string, String correo2, String ip2) {
+		// TODO Auto-generated constructor stub
+	}
 private long id;
 private String nombre, correo, ip;
 // Ctor(), Get/Set, toString(), …
