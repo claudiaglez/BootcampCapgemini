37e85222c8048de043eae62b02e8328d471a3ebc
claudiaglez <claudia.glezg@gmail.com>
2025-03-20 16:17:48 +0100

    Add post method in CategoriesResource

--- catalogo/src/main/java/com/example/application/resources/CategoriesResource.java
+++ catalogo/src/main/java/com/example/application/resources/CategoriesResource.java
@@ -1,23 +1,33 @@
 package com.example.application.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.domains.contracts.services.CategoriesService;
 import com.example.domains.entities.models.CategoryDTO;
+import com.example.exceptions.BadRequestException;
+import com.example.exceptions.DuplicateKeyException;
+import com.example.exceptions.InvalidDataException;
 import com.example.exceptions.NotFoundException;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/categorías/v1")
@@ -50,6 +60,16 @@ public class CategoriesResource {
 			throw new NotFoundException("No se encontró la categoría con id " + id);
 		}
 		return CategoryDTO.from(item.get());
+	}
+	
+	@PostMapping
+	@Operation(summary = "Crea una categoría")
+	@ApiResponse(responseCode = "201", description = "Categoría creada")
+	public ResponseEntity<Object> create(@Valid @RequestBody CategoryDTO item) throws BadRequestException, DuplicateKeyException, InvalidDataException {
+		var newItem = categoriesService.add(CategoryDTO.from(item));
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+			.buildAndExpand(newItem.getCategoryId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 	
 	
