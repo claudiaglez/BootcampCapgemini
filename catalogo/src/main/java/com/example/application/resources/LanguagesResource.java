85797d40c1b91a62c8c9cae9e25b7eb0236a27fe
claudiaglez <claudia.glezg@gmail.com>
2025-03-20 16:49:22 +0100

    Create get method by id in the LanguagesResource

--- catalogo/src/main/java/com/example/application/resources/LanguagesResource.java
+++ catalogo/src/main/java/com/example/application/resources/LanguagesResource.java
@@ -3,13 +3,17 @@ package com.example.application.resources;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domains.contracts.services.LanguagesService;
+import com.example.domains.entities.models.CategoryDTO;
 import com.example.domains.entities.models.LanguageDTO;
+import com.example.exceptions.NotFoundException;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -28,5 +32,16 @@ public class LanguagesResource {
 	public List<LanguageDTO> getAll() {
 		return languagesService.getByProjection(LanguageDTO.class);
 	}
+	
+	@GetMapping(path = "/{id}")
+	@Operation(summary = "Obtiene un idioma por su id")
+	public LanguageDTO getOne(@PathVariable @Parameter(description = "Identificador del idioma") int id) throws NotFoundException {
+		var item = languagesService.getOne(id);
+		if (item.isEmpty()) {
+			throw new NotFoundException("No se encontró el idioma con id " + id);
+		}
+		return LanguageDTO.from(item.get());
+	}
+	
 
 }
