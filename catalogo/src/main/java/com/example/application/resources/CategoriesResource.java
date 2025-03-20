2300a0afef8e4e0101cabb4cbf7e99a7a9ba84d4
claudiaglez <claudia.glezg@gmail.com>
2025-03-20 16:21:48 +0100

    Add delete method to the CategoriesResource

--- catalogo/src/main/java/com/example/application/resources/CategoriesResource.java
+++ catalogo/src/main/java/com/example/application/resources/CategoriesResource.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.P
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -89,6 +90,15 @@ public class CategoriesResource {
 		categoriesService.modify(CategoryDTO.from(item));
 	}
 	
+	@DeleteMapping("/{id}")
+	@Operation(summary = "Borra una categoría por su id")
+	@ApiResponse(responseCode = "204", description = "Categoría a borrar")
+	@ApiResponse(responseCode = "404", description = "Categoría no encontrada")
+	@ApiResponse(responseCode = "400", description = "ID inválido proporcionado")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable int id) {
+		categoriesService.deleteById(id);
+	}
 	
 
 }
