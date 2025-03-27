e4846165ed89c223707c72a41fbdd52399349618
claudiaglez <claudia.glezg@gmail.com>
2025-03-27 21:22:28 +0100

    Delete unimplemented method in LanguagesServiceTest

--- catalogo/src/test/java/com/example/services/LanguagesServiceTest.java
+++ catalogo/src/test/java/com/example/services/LanguagesServiceTest.java
@@ -153,30 +153,7 @@ public class LanguagesServiceTest {
         verify(languagesRepository).delete(language);
     }
     
-    @Test
-    @DisplayName("Obtener idiomas que acaban en N")
-    public void testIdiomasAcabanN() {
-        Language spanish = new Language();
-        spanish.setName("Spanish");
-
-        Language english = new Language();
-        english.setName("English");
-
-        Language german = new Language();
-        german.setName("German");
-
-        List<Language> idiomasMock = Arrays.asList(spanish, english, german);
-
-        when(languagesRepository.findByNameEndingWith("n")).thenReturn(idiomasMock);
-
-        List<Language> result = languagesService.idiomasAcabanN("n");
-
-        assertNotNull(result);
-        assertEquals(3, result.size());
-        assertTrue(result.stream().anyMatch(idioma -> idioma.getName().equals("Spanish")));
-        assertTrue(result.stream().anyMatch(idioma -> idioma.getName().equals("English")));
-        assertTrue(result.stream().anyMatch(idioma -> idioma.getName().equals("German")));
-    }
+    
     
    
     
