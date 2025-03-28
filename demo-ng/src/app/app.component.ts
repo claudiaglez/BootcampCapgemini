82ac11a0d8242fa0ef400f37bf7fd0ac9bc47c03
claudiaglez <claudia.glezg@gmail.com>
2025-03-28 13:42:35 +0100

    Create notification modal component

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoggerService } from '@my/core';
-import { NotificationComponent } from './main/notification/notification.component';
 import { DemosComponent } from './demos/demos.component';
+import { NotificationModalComponent } from './main';
 
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, NotificationComponent, DemosComponent,],
+  imports: [CommonModule, RouterOutlet, NotificationModalComponent, DemosComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
