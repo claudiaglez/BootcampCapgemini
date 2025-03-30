5b97e8aa367503890db8fae39ed968621d8cec0e
claudiaglez <claudia.glezg@gmail.com>
2025-03-30 13:41:31 +0200

    Fix FilmsService implementation to delete methods and unify the Film entity

--- catalogo/src/main/java/com/example/domains/services/FilmsServiceImpl.java
+++ catalogo/src/main/java/com/example/domains/services/FilmsServiceImpl.java
@@ -1,9 +1,7 @@
 package com.example.domains.services;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.domains.contracts.repositories.FilmsRepository;
 import com.example.domains.contracts.services.FilmsService;
-import com.example.domains.entities.Actor;
 import com.example.domains.entities.Film;
-import com.example.domains.entities.FilmActor;
 import com.example.domains.entities.models.FilmDetailsDTO;
 import com.example.exceptions.DuplicateKeyException;
 import com.example.exceptions.InvalidDataException;
@@ -27,6 +23,10 @@ import jakarta.validation.Valid;
 public class FilmsServiceImpl implements FilmsService {
 	@Autowired
 	private FilmsRepository filmsRepository;
+	
+	public FilmsServiceImpl(FilmsRepository filmsRepository) {
+		this.filmsRepository = filmsRepository;
+	}
 
 
 	@Override
@@ -56,36 +56,11 @@ public class FilmsServiceImpl implements FilmsService {
             throw new InvalidDataException("La película no puede ser nula");
         }
         
-        Film existingFilm = filmsRepository.findById(item.getFilmId())
-                .orElseThrow(() -> new NotFoundException("Película no encontrada"));
-
-        existingFilm.setTitle(item.getTitle());
-        existingFilm.setDescription(item.getDescription());
-        existingFilm.setReleaseYear(item.getReleaseYear());
-     
-        updateFilmActors(existingFilm, item.getActors());
-
-        return filmsRepository.save(existingFilm);
-    }
-
-    private void updateFilmActors(Film film, List<Actor> actors) {
-        Set<FilmActor> currentFilmActors = new HashSet<>(film.getFilmActors());
-
-        for (FilmActor filmActor : currentFilmActors) {
-            if (!actors.contains(filmActor.getActor())) {
-                film.removeActor(filmActor.getActor());
-            }
-        }
-
-        for (Actor actor : actors) {
-            if (film.getActors().stream().noneMatch(existingActor -> existingActor.getActorId() == actor.getActorId())) {
-                film.addActor(actor);
-            }
-        }
-    }
-
-
-
+        if(item.isInvalid())
+			throw new InvalidDataException(item.getErrorsMessage(), item.getErrorsFields());
+		var read = filmsRepository.findById(item.getFilmId()).orElseThrow(() -> new NotFoundException());
+		return filmsRepository.save(item.merge(read));
+	}
 
     @Override
     public void delete(@Valid Film item) throws InvalidDataException {
