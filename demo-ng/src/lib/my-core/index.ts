26f935c0522e5e3bec7d2d42ddd197e158876ddf
claudiaglez <claudia.glezg@gmail.com>
2025-04-01 15:51:36 +0200

    Create sizer component

--- demo-ng/src/lib/my-core/index.ts
+++ demo-ng/src/lib/my-core/index.ts
@@ -1,2 +1,4 @@
 export { MyCoreModule } from './my-core.module';
 export * from './services/logger.service';
+export * from './pipes/cadenas.pipe';
+export * from './components/sizer/sizer.component';
